c41c5ba18f83fc78da616a659b767b64e4eb8891
derrick b <dbozich1@gmail.com>
2018-04-25 23:31:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,10 @@ def display_board(board)
   bound = "|"
   puts " "+board[0]+" "+bound+" "+board[1]+" "+bound+" "+board[2]
   puts line
+  puts " "+board[3]+" "+bound+" "+board[4]+" "+bound+" "+board[5]
+  puts line
+  puts " "+board[6]+" "+bound+" "+board[7]+" "+bound+" "+board[8]
+
 end
 
-display_board(["X"," ","X"])
+display_board(["X"," ","X","X"," ","X","X"," ","X"])
