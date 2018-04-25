66380b3920c64d7788c4ca1d47363829d91024c3
derrick b <dbozich1@gmail.com>
2018-04-25 23:35:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 def display_board(board)
   line = "-----------"
   bound = "|"
-  puts " "+board[0]+" "+bound+" "+board[1]+" "+bound+" "+board[2]
+  puts " "+board[0]+" "+bound+" "+board[1]+" "+bound+" "+board[2]+" "
   puts line
-  puts " "+board[3]+" "+bound+" "+board[4]+" "+bound+" "+board[5]
+  puts " "+board[3]+" "+bound+" "+board[4]+" "+bound+" "+board[5]+" "
   puts line
-  puts " "+board[6]+" "+bound+" "+board[7]+" "+bound+" "+board[8]
+  puts " "+board[6]+" "+bound+" "+board[7]+" "+bound+" "+board[8]+" "
 
 end
 
